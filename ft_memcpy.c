6b84d532baa3d429e0d7674d0c9df875a61d064a
Elena Reina Lopez <ereina-l@c1r4s1.42malaga.com>
2024-09-18 13:58:38 +0200

    memcopy

---
+++ ft_memcpy.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ereina-l <ereina-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 11:23:56 by ereina-l          #+#    #+#             */
+/*   Updated: 2024/09/18 11:48:45 by ereina-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+	char	*d;
+	char	*s;
+
+	if (!dst && !src)
+		return (0);
+	i = 0;
+	d = (char *)dst;
+	s = (char *)src;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dst);
+
+}
+
+/* #include <stdio.h>
+int	main(void)
+{
+	char	dst[4];
+	char	src[] = "Javier";
+
+	ft_memcpy(dst, src, 4);
+	printf("%s\n", dst);
+	return (0);
+} */
