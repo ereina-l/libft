636784b1fef85ea19b2a91a6ba77986ce403639d
ereina-l <ereina-l@student.42malaga.com>
2024-09-24 14:21:14 +0200

    calloc

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ereina-l <ereina-l@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/09/21 17:56:53 by ereina-l          #+#    #+#              #
-#    Updated: 2024/09/21 19:18:55 by ereina-l         ###   ########.fr        #
+#    Updated: 2024/09/24 14:05:44 by ereina-l         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,7 +19,7 @@ SRC = ft_atoi.c ft_bzero.c ft_isalnum.c ft_isalpha.c \
 	ft_memcmp.c ft_memcpy.c ft_memmove.c ft_memset.c \
 	ft_strchr.c ft_strlcat.c ft_strlcpy.c ft_strlen.c \
 	ft_strncmp.c ft_strnstr.c ft_strrchr.c ft_tolower.c \
-	ft_toupper.c ft_strlcat.c
+	ft_toupper.c ft_strlcat.c ft_calloc.c
 
 OBJ = $(SRC:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ereina-l <ereina-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 16:17:44 by ereina-l          #+#    #+#             */
-/*   Updated: 2024/09/21 19:18:43 by ereina-l         ###   ########.fr       */
+/*   Updated: 2024/09/24 14:05:32 by ereina-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void    *ft_calloc(size_t count, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
