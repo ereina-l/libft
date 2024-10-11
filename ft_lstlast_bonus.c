7731d8ad21b561292199409e1e51a762769b3bd8
Elena Reina Lopez <ereina-l@c3r2s1.42malaga.com>
2024-10-11 17:16:29 +0200

    2 more bonus ft, passed paco

---
+++ ft_lstlast_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ereina-l <ereina-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 17:11:51 by ereina-l          #+#    #+#             */
+/*   Updated: 2024/10/11 17:14:43 by ereina-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*ptr;
+
+	ptr = lst;
+	if (!ptr)
+		return (NULL);
+	while (ptr->next)
+	{
+		ptr = ptr->next;
+	}
+	return (ptr);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ereina-l <ereina-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 16:17:44 by ereina-l          #+#    #+#             */
-/*   Updated: 2024/10/11 17:02:31 by ereina-l         ###   ########.fr       */
+/*   Updated: 2024/10/11 17:15:12 by ereina-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,7 @@ typedef struct s_list
 
 t_list	*ft_lstnew(void *content);	
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
